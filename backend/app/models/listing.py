f8f2a711123fb5db674ec92ac530db277b1b8636
FNU Shaliwa <sf452@cornell.edu>
2024-01-01 09:05:37 -0500

    Implemented listing.py and review.py

---
+++ backend/app/models/listing.py
@@ -0,0 +1,55 @@
+from . import db
+import datetime
+
+class Listing(db.Model):
+  """
+  Listing Model: represents a table for product listings in the database.
+  """
+  __tablename__ = "listings"
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  title = db.Column(db.String, nullable = False)
+  description = db.Column(db.String, nullable = False)
+  price = db.Column(db.Numeric(precision = 8, scale = 2), nullable = False)
+  category = db.Column(db.String, nullable = False)
+  image_url = db.Column(db.String, nullable = True)
+  listed_time = db.Column(db.DateTime, nullable = False)
+  seller_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+  
+  def __init__(self, seller_id, **kwargs):
+    """
+    Initialize a listing object. 
+    """
+    self.title = kwargs.get("title")
+    self.description = kwargs.get("description")
+    self.price = kwargs.get("price")
+    self.category = kwargs.get("category")
+    self.image_url = kwargs.get("image_url")
+    self.listed_time = datetime.utcnow()
+    self.seller_id = seller_id
+    
+  def serialize(self):
+    """
+    Serialize a listing object.
+    """
+    return {
+      "title": self.title,
+      "description": self.description,
+      "price": self.price,
+      "category": self.category,
+      "image_url": self.image_url,
+      "listed_time": self.listed_time,
+      "seller_id": self.seller_id
+    }
+    
+  def simple_serialize(self):
+    """
+    Serialize a listing object without listed time and seller id.
+    """
+    return {
+      "title": self.title,
+      "description": self.description,
+      "price": self.price,
+      "category": self.category,
+      "image_url": self.image_url
+    }  
+  
---
+++ backend/app/models/review.py
@@ -0,0 +1,52 @@
+from . import db
+import datetime
+
+class Review(db.Model):
+  """
+  Review Model: represents a table for reviews in the database.
+  """
+  __tablename__ = "reviews"
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+  reviewer = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+  reviewee = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+  item = db.Column(db.Integer, db.ForeignKey("listings.id"), nullable = False)
+  rating = db.Column(db.Integer, nullable = False)
+  text = db.Column(db.String, nullable = True)
+  review_time = db.Column(db.DateTime, nullable = False)
+  
+  def __init__(self, reviewer, reviewee, item, **kwargs):
+    """
+    Initialize a review object. 
+    """
+    self.reviewer = reviewer
+    self.reviewee = reviewee
+    self.item = item
+    self.rating = kwargs.get("rating")
+    self.text = kwargs.get("text")
+    self.review_time = datetime.utcnow()
+    
+  def serialize(self):
+    """
+    Serialize a review object.
+    """
+    return {
+      "reviewer": self.reviewer,
+      "reviewee": self.reviewee,
+      "item": self.item,
+      "rating": self.rating,
+      "text": self.text,
+      "review_time": self.review_time
+    }
+    
+  def simple_serialize(self):
+    """
+    Serialize a review object without reviewer and reviewee.
+    """
+    return {
+      "item": self.item,
+      "rating": self.rating,
+      "text": self.text,
+      "review_time": self.review_time
+
+    }  
+  
